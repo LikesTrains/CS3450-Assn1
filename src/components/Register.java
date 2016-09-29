b4da6fa18cf3a7f2983c243692d284913c11955e
GrantRussell <grant.s.russ@gmail.com>
2016-09-29 15:15:57 -0600

    Register implementation
    Added functionality with register page.

--- src/components/Register.java
+++ src/components/Register.java
@@ -1,23 +1,33 @@
 package components;
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 
-public class Register extends JFrame implements ActionListener {
+public class Register extends JFrame implements ActionListener{
 	private Container pane;
 	private JButton Checkout, BackOffice;
 	private JPanel ButtonContainer;
 	private User admin, currentUser = new User (0,"","",-1,0);
 	private static final long serialVersionUID = 1L;
+	private boolean registerActive = false;
 	
 	Register(){
 		this.setTitle("Register");
@@ -59,6 +69,10 @@ public class Register extends JFrame implements ActionListener {
 	    JPanel myPanel = new JPanel();
 	    myPanel.add(new JLabel("ID:"));
 	    myPanel.add(IDField);
+	    IDField.requestFocus();
+	    revalidate();
+	    pack();
+	    IDField.getCursor();
 	    myPanel.add(Box.createHorizontalStrut(15)); // a spacer
 	    myPanel.add(new JLabel("Password:"));
 	    myPanel.add(PassField);
@@ -92,23 +106,101 @@ public class Register extends JFrame implements ActionListener {
 		    }
 		    //User is valid
 		    else {
-		    currentUser = searchUser(ID, password);
+		    currentUser.clone(searchUser(ID, password));
 		    JOptionPane.showMessageDialog(null, "Login Successful.");
 		    }		    
 	    }
-	    //if (Integer.parseInt(xField.getText())==0){
-	    	
-	    //}
-	    
-		//String inputValue = JOptionPane.showInputDialog("Input your used ID");
 	}
 	
 	void displayReigster(){
 		if (currentUser.getID()==-1) {
+			currentUser.reset();
 			return;
 		}
-		
-			System.out.println("Yeah");
+			registerActive = true;
+			//Highest Level, Contains all elements in the register.
+			JPanel registerGUI = new JPanel(new BorderLayout());
+			registerGUI.setPreferredSize(new Dimension(250,300));
+			
+			//Mid Level, split input outputs into two sections.
+			JPanel outputWindow = new JPanel(new FlowLayout());
+			
+			JPanel inputWindow = new JPanel(new FlowLayout());
+			inputWindow.setPreferredSize(new Dimension(250,100));
+			
+			//Elements of the output window
+			JTextArea itemWindow = new JTextArea();
+			itemWindow.setPreferredSize(new Dimension(105,420));
+			itemWindow.setEditable(false);
+			itemWindow.append("Part Number\n------------------------------\n");
+			JTextArea quantityWindow = new JTextArea();
+			quantityWindow.setPreferredSize(new Dimension(50,420));
+			quantityWindow.setEditable(false);
+			quantityWindow.append("Quantity\n--------------\n");
+			JTextArea totalWindow = new JTextArea();
+			totalWindow.setPreferredSize(new Dimension(50,420));
+			totalWindow.setEditable(false);
+			totalWindow.append("Total\n--------------\n");
+			
+			//JScrollPane scrollWindow = new JScrollPane(itemWindow, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+			
+			//Elements of the input window
+			JLabel itemLabel = new JLabel("Part #: ");
+			JTextField itemField = new JTextField(8);
+			JLabel quantityLabel = new JLabel("Quantity: ");
+			JTextField quantityField = new JTextField(2);
+			JButton checkoutButton = new JButton("Checkout");
+			checkoutButton.setPreferredSize(new Dimension(250,75));
+			
+			//Add elements of inputWindow into inputWindow.
+			inputWindow.add(itemLabel);
+			inputWindow.add(itemField);
+			inputWindow.add(quantityLabel);
+			inputWindow.add(quantityField);
+			inputWindow.add(checkoutButton);
+			
+			//Add elements of outputWindow into outputWindow
+			outputWindow.add(itemWindow);
+			outputWindow.add(quantityWindow);
+			outputWindow.add(totalWindow);
+			
+			//Add input and output windows into registerGUI.
+			registerGUI.add(outputWindow, BorderLayout.CENTER);
+			registerGUI.add(inputWindow, BorderLayout.SOUTH);
+			
+			ActionListener itemQuantityListener = new ActionListener(){
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					Dimension dim = itemWindow.getSize();
+					int dimx = (int) dim.getWidth();
+					itemWindow.append(itemField.getText() + "\n");
+					quantityWindow.append("x "+ quantityField.getText() + "\n");
+					totalWindow.append("$0.00\n");
+					itemField.setText("");
+					quantityField.setText("");
+					itemField.requestFocus();
+				}
+			};
+			
+			itemField.addActionListener(itemQuantityListener);
+			quantityField.addActionListener(itemQuantityListener);
+			checkoutButton.addActionListener(new ActionListener(){
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					// TODO Auto-generated method stub
+					pane.remove(registerGUI);
+					pane.add(ButtonContainer);
+					pane.revalidate();
+					pack();
+				}
+			});
+			
+			pane.remove(ButtonContainer);
+			pane.add(registerGUI);
+			pane.revalidate();
+			pack();
+			currentUser.reset();
 	}
 	
 	@Override
@@ -123,6 +215,7 @@ public class Register extends JFrame implements ActionListener {
 			deployLogin();
 		}
 	}
+	
 	/**
 	 * If user is not found a dummy user with an ID of -1 will be returned
 	 * @return User object that matches ID and password from the database.
@@ -132,5 +225,5 @@ public class Register extends JFrame implements ActionListener {
 			return admin;
 		}
 		return new User(1,"Yo","Dawg",-1,47);
-	};
+	}
 }
